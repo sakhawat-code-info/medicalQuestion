1bee2e24b960b7465fd8b0c09709d8c8f4ef0a8f
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-02-15 10:35:41 +0600

    basic file added

---
+++ script/script.js
@@ -0,0 +1,24 @@
+
+const treatment = {
+    eye: {
+        stye: {
+            cc: ['cough', 'thanda', 'kashi'],
+            investigation: ['cbc', 'x-ray'],
+            rx: {
+                1: 'Tab. cipro',
+                2: 'Tab. Metro'
+            },
+            advice: {
+                1: 'no need food',
+                2: 'drinking water'
+            }
+        }
+        shoreThroat: {
+
+        }
+    }
+}
+
+console.log(treatment.eye.stye);
+
+
